8767527a0ea337845b861e285f2e12be5d8771d1
Nikita Doroshenko <nikitokd05@gmail.com>
2025-09-05 12:09:00 +0300

    fixed solution

--- src/components/PeopleLink/PeopleLink.tsx
+++ src/components/PeopleLink/PeopleLink.tsx
@@ -1,58 +1,20 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Person } from '../../types';
 import classNames from 'classnames';
 
 type Props = {
-  people: Person[];
   person: Person;
 };
 
-export const PeopleLink: React.FC<Props> = ({ person, people }) => {
-  const mother = people.find(p => p.name === person.motherName);
-  const father = people.find(p => p.name === person.fatherName);
-  const { slug } = useParams();
-
+export const PeopleLink: React.FC<Props> = ({ person }) => {
   return (
-    <tr
-      data-cy="person"
+    <Link
+      to={`/people/${person.slug}`}
       className={classNames({
-        'has-background-warning': slug === person.slug,
+        'has-text-danger': person.sex === 'f',
       })}
     >
-      <td>
-        <Link
-          to={`/people/${person.slug}`}
-          className={classNames({
-            'has-text-danger': person.sex === 'f',
-          })}
-        >
-          {person.name}
-        </Link>
-      </td>
-
-      <td>{person.sex}</td>
-      <td>{person.born}</td>
-      <td>{person.died}</td>
-      <td>
-        {!person.motherName ? (
-          '-'
-        ) : mother ? (
-          <Link to={`/people/${mother.slug}`} className="has-text-danger">
-            {person.motherName}
-          </Link>
-        ) : (
-          person.motherName
-        )}
-      </td>
-      <td>
-        {!person.fatherName ? (
-          '-'
-        ) : father ? (
-          <Link to={`/people/${father.slug}`}>{person.fatherName}</Link>
-        ) : (
-          person.fatherName
-        )}
-      </td>
-    </tr>
+      {person.name}
+    </Link>
   );
 };
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,11 +1,15 @@
+import { useParams } from 'react-router-dom';
 import { Person } from '../../types';
 import { PeopleLink } from '../PeopleLink';
+import classNames from 'classnames';
 
 type Props = {
   people: Person[];
 };
 
 export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const { slug } = useParams();
+
   return (
     <table
       data-cy="peopleTable"
@@ -23,9 +27,46 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
       </thead>
 
       <tbody>
-        {people.map(person => (
-          <PeopleLink person={person} people={people} key={person.name} />
-        ))}
+        {people.map(person => {
+          const mother = people.find(p => p.name === person.motherName);
+          const father = people.find(p => p.name === person.fatherName);
+
+          return (
+            <tr
+              data-cy="person"
+              className={classNames({
+                'has-background-warning': slug === person.slug,
+              })}
+              key={person.name}
+            >
+              <td>
+                <PeopleLink person={person} />
+              </td>
+
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+              <td>
+                {!person.motherName ? (
+                  '-'
+                ) : mother ? (
+                  <PeopleLink person={mother} />
+                ) : (
+                  person.motherName
+                )}
+              </td>
+              <td>
+                {!person.fatherName ? (
+                  '-'
+                ) : father ? (
+                  <PeopleLink person={father} />
+                ) : (
+                  person.fatherName
+                )}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
